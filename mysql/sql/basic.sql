ff75f135dbac5c6f5fc95ee5795bdee5c1c9301c
jeongminie <opooip@nate.com>
2022-08-02 19:43:27 +0900

    220802

--- mysql/sql/basic.sql
+++ mysql/sql/basic.sql
@@ -1,1 +1,15 @@
-dd
+SELECT * FROM `user` u 
+WHERE 1=1 
+-- AND name LIKE '%5'
+-- AND name LIKE 'a%'
+AND name LIKE '%dm%';
+
+SELECT * FROM `user` u 
+WHERE 1=1
+-- AND userSeq > 5
+-- AND userSeq >=5
+AND userSeq BETWEEN 1 and 7;
+
+SELECT * FROM `user` u 
+WHERE 1=1
+AND userId IS NULL;
